8f6a16c7ea15b4ee9aea590ba673b90b18b28275
Kena <kenaebustos@gmail.com>
2024-01-27 23:38:02 -0500

    Update ContactSetviceTest and ContactTest

---
+++ ContactService/src/test/ContactServiceTest.java
@@ -0,0 +1,7 @@
+package test;
+
+public class ContactServiceTest {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ ContactService/src/test/ContactTest.java
@@ -0,0 +1,10 @@
+package test;
+import static org.junit.jupiter.api.Assertions.t;
+
+
+class ContactTest {
+	@Test
+	void testContactClass() {
+		Contact contact = new Contact();
+	}
+}
